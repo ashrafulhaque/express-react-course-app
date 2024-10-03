73cafb7f2dbd5ae06ca674d84104b30b63c6dc64
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-10-04 01:00:53 +0600

    Initial commit: Vite,Tailwind, DaisyUI,React Router DOm and FontAwesome installed.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-orange-600 text-4xl">
+        {" "}
+        <i class="fa-solid fa-user"></i>
+        Hello World! <i class="fa-brands fa-github-square"></i>
+      </h1>
+    </>
+  );
+}
+
+export default App;
